ff0ebf0b89a9e0e915f8af1e202eedb15979e8e2
Muhammad Farhan Al Hasan <farhanlhsn@gmail.com>
2025-04-23 12:13:08 +0000

    initial commit

---
+++ backend/src/main/java/com/TryCatch/NusaCart/NusaCartApplication.java
@@ -0,0 +1,13 @@
+package com.TryCatch.NusaCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusaCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NusaCartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/TryCatch/NusaCart/NusaCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TryCatch.NusaCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NusaCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
