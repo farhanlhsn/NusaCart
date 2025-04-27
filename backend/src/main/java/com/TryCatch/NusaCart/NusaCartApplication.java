da2b003352a0fc4347bb0cf8e41e1fc4572c203e
zazazaxky-m <athallahzacky0204@gmail.com>
2025-04-27 11:33:05 +0700

    set database

--- backend/src/main/java/com/TryCatch/NusaCart/NusaCartApplication.java
+++ backend/src/main/java/com/TryCatch/NusaCart/NusaCartApplication.java
@@ -8,6 +8,7 @@ public class NusaCartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NusaCartApplication.class, args);
+		System.out.println("Hello NusaCart");
 	}
 
 }
